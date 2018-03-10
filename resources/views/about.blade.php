b91250a468489ae5dd69f4a16ada9da512bc11be
Orxan <baku_orxan17@mail.ru>
2018-03-10 14:05:57 +0400

    fixed bugs

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+
+@section('content')
+<h1>About</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, nemo, quas! Libero voluptates quis neque quam ratione ipsa labore earum qui provident amet, non harum illo asperiores architecto. Illo, reprehenderit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora rem laudantium, tenetur? Molestiae architecto dolor eveniet sint, voluptatibus error et aspernatur? Dolorum in, vero impedit saepe vel qui labore ea.</p>
+@endsection
---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron text-center">
+	<div class="container">
+		<h1>Welcome to our Site</h1>
+		<p class="lead">Welcome to our brand new Laravel website. Laravel version 5.4 used</p>
+	</div>
+</div>
